1a42000b3b1918d070c46e52b38cf633acea875f
Ben Stoica <bennystoica@gmail.com>
2025-05-18 16:31:13 -0400

    name change

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -101,8 +101,8 @@ const AuthForm = (props: AuthFormProps) => {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <Image src="./logo.svg" alt="rehearseai" height={32} width={38} />
-          <h2 className="text-primary-100">RehearseAI</h2>
+          <Image src="./logo.svg" alt="interview prep" height={32} width={38} />
+          <h2 className="text-primary-100">Interview Prep</h2>
         </div>
 
         <h3>Practice job interviews with AI.</h3>
